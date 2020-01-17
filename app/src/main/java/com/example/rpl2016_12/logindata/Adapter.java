a7c9f5825af16ee25680e091858fe6b6ec60e006
wulanayu <sitidiyah.xrpl2017smkrus@gmail.com>
2020-01-17 10:31:47 +0700

    p

---
+++ app/src/main/java/com/example/rpl2016_12/logindata/Adapter.java
@@ -0,0 +1,4 @@
+package com.example.rpl2016_12.logindata;
+
+public class Adapter {
+}
---
+++ app/src/main/java/com/example/rpl2016_12/logindata/Registrasi.java
@@ -0,0 +1,13 @@
+package com.example.rpl2016_12.logindata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registrasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrasi);
+    }
+}
